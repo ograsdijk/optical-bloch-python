9f0271291438e3aa9e0e2a2a31e673010648f00d
ograsdijk <o.grasdijk@gmail.com>
2020-01-27 19:20:08 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name = "optical_bloch",
+    author = "Olivier Grasdijk",
+    author_email = "olivier.grasdijk@yale.edu",
+    description = "package for setting up and solving a ODE system of optical Bloch equations",
+    url = "https://github.com/",
+    packages = setuptools.find_packages(),
+    python_requires = '>=3.6',
+    version = "0.1"
+)
